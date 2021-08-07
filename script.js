281581e4f74d652fe546a6fac563cb8757cf6c4b
Kara <karahuynh6@hotmail.com>
2021-08-06 21:57:00 -0400

    added in nav bar, second cat page

--- script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("dropdown").style.width = "245px";
-    document.getElementById("dropdown").style.opacity = "0.9";
-    document.getElementById("openbtn").style.opacity = "0.9";
-  }
-  
-  function closeNav() {
-    document.getElementById("dropdown").style.width = "0";
-  }
