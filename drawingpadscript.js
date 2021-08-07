3b25be8ed4e64b5808523e53e68f62d6792796f6
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2021-08-07 17:10:32 -0400

    Update drawingpadscript.js

--- drawingpadscript.js
+++ drawingpadscript.js
@@ -1,44 +1,64 @@
 var canvas = document.getElementById('paint');
 var ctx = canvas.getContext('2d');
  
-var sketch = document.getElementById('sketch');
-var sketch_style = getComputedStyle(sketch);
-canvas.width = 1500;
-canvas.height = 1250;
 
-var mouse = {x: 0, y: 0};
- 
-/* Mouse Capturing Work */
-canvas.addEventListener('mousemove', function(e) {
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
-}, false);
-
-/* Drawing on Paint App */
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-
-ctx.strokeStyle = "red";
-function getColor(colour){ctx.strokeStyle = colour;}
+  function getColor(colour){ctx.strokeStyle = colour;}
 
 function getSize(size){ctx.lineWidth = size;}
+  
+
+// The event handler for any changes made to the tool selector
+
+ var canvas = document.getElementById("draw");
+
+var ctx = canvas.getContext("2d");
+resize();
+
+var canvas = document.getElementById('paint');
+var ctx = canvas.getContext('2d');
+ 
+var sketch = document.getElementById('sketch');
+var sketch_style = getComputedStyle(sketch);
+canvas.width = 600;
+canvas.height = 550;
+// resize canvas when window is resized
+function resize() {
+ ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+
+// initialize position as 0,0
+var pos = { x: 0, y: 0 };
+
+// new position from mouse events
+function setPosition(e) {
+  pos.x = e.clientX;
+  pos.y = e.clientY;
+}
+
+function draw(e) {
+  if (e.buttons !== 1) return; // if mouse is not clicked, do not go further
+
+  var color = document.getElementById("hex").value;
+
+  ctx.beginPath(); // begin the drawing path
+
+  // width of line
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = color; // hex color of line
+
+  ctx.moveTo(pos.x, pos.y); // from position
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to position
+
+  ctx.stroke(); // draw it!
+}
 
 
-//ctx.strokeStyle = 
-//ctx.strokeStyle = document.settings.colour[1].value;
- 
-canvas.addEventListener('mousedown', function(e) {
-    ctx.beginPath();
-    ctx.moveTo(mouse.x, mouse.y);
- 
-    canvas.addEventListener('mousemove', onPaint, false);
-}, false);
- 
-canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-}, false);
- 
-var onPaint = function() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-};
+// add window event listener to trigger when window is resized
+window.addEventListener("resize", resize);
+
+// add event listeners to trigger on different mouse events
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
