0862ba219abef53cbf54210d38a4875320053fcb
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2021-08-07 16:02:35 -0400

    catdrawingpad fixes

--- script.js
+++ script.js
@@ -32,11 +32,12 @@ var canvas = document.getElementById('paint');
 var ctx = canvas.getContext('2d');
  
 
-function getColor(colour){ctx.strokeStyle = colour;}
+  function getColor(colour){ctx.strokeStyle = colour;}
 
 function getSize(size){ctx.lineWidth = size;}
+  
 
-// canvas
+// The event handler for any changes made to the tool selector
 
  var canvas = document.getElementById("draw");
 
@@ -50,35 +51,37 @@ var sketch = document.getElementById('sketch');
 var sketch_style = getComputedStyle(sketch);
 canvas.width = 600;
 canvas.height = 550;
-// resize
+// resize canvas when window is resized
 function resize() {
  ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
 }
+
+// initialize position as 0,0
 var pos = { x: 0, y: 0 };
 
-// mouse
+// new position from mouse events
 function setPosition(e) {
   pos.x = e.clientX;
   pos.y = e.clientY;
 }
 
 function draw(e) {
-  if (e.buttons !== 1) return; // stop when mouse is not clicked
+  if (e.buttons !== 1) return; // if mouse is not clicked, do not go further
 
   var color = document.getElementById("hex").value;
 
-  ctx.beginPath(); // drawing path
+  ctx.beginPath(); // begin the drawing path
 
   // width of line
-  ctx.lineCap = "round"; 
-  ctx.strokeStyle = color; // hex color 
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = color; // hex color of line
 
   ctx.moveTo(pos.x, pos.y); // from position
   setPosition(e);
   ctx.lineTo(pos.x, pos.y); // to position
 
-  ctx.stroke(); //stroke
+  ctx.stroke(); // draw it!
 }
 
 
@@ -89,4 +92,3 @@ window.addEventListener("resize", resize);
 document.addEventListener("mousemove", draw);
 document.addEventListener("mousedown", setPosition);
 document.addEventListener("mouseenter", setPosition);
-
