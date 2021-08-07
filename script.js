633198e4c80085ff5c95459e92a1dfb9e6c903f5
Kara <karahuynh6@hotmail.com>
2021-08-06 20:43:41 -0400

    added side menu, must create it to dropdown

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("dropdown").style.width = "245px";
+    document.getElementById("dropdown").style.opacity = "0.9";
+    document.getElementById("openbtn").style.opacity = "0.9";
+  }
+  
+  function closeNav() {
+    document.getElementById("dropdown").style.width = "0";
+  }
